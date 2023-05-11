b686271b53d64c7ff8cdcac0ddf2539f0cfb8d4b
Muhammad-Saad0 <119536957+Muhammad-Saad0@users.noreply.github.com>
2023-05-11 18:10:03 +0500

    create post (completed)

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,5 +1,29 @@
+import { connectToDatabase } from "@lib/mongodb";
+import Prompt from "@models/prompt";
+
 export async function POST(req) {
-  const data = await req.json();
-  console.log(data);
-  new Response({ message: "Hello world!!" });
+  const { prompt, tag, user } = await req.json();
+  const userId = user.id;
+  try {
+    await connectToDatabase();
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt: prompt,
+      tag: tag,
+    });
+    await newPrompt.save();
+    return new Response(
+      JSON.stringify(newPrompt),
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    return new Response(
+      "failed to create new Response",
+      {
+        status: 500,
+      }
+    );
+  }
 }
